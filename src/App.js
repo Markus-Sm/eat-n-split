0c55045ccfc60ee02d0dc9f4b999c2fa8531cf1f
Marek Samofal <marek.samofal@gmail.com>
2024-03-19 14:28:17 +0100

    building static app: Forms

--- src/App.js
+++ src/App.js
@@ -13,12 +13,73 @@ const initialFriends = [
 	},
 	{
 		id: 499476,
-		name: 'Anthony!',
+		name: 'Anthony',
 		image: 'https://i.pravatar.cc/48?u=499476',
 		balance: 0,
 	},
 ]
 
 export default function App() {
-	return <div>Hello!</div>
+	return (
+		<div className='app'>
+			<div className='sidebar'>
+				<FriendsList />
+			</div>
+		</div>
+	)
+}
+
+function FriendsList() {
+	const friends = initialFriends
+
+	return (
+		<ul>
+			{friends.map(friend => (
+				<Friend friend={friend} key={friend.id} />
+			))}
+		</ul>
+	)
+}
+
+function Friend({ friend }) {
+	return (
+		<li>
+			<img src={friend.image} alt={friend.name} />
+			<h3>{friend.name}</h3>
+
+			{friend.balance < 0 && (
+				<p className='red'>
+					You owe {friend.name} {Math.abs(friend.balance)}€
+				</p>
+			)}
+
+			{friend.balance > 0 && (
+				<p className='green'>
+					{friend.name} owes you {Math.abs(friend.balance)}€
+				</p>
+			)}
+
+			{friend.balance === 0 && <p>You and {friend.name} are even</p>}
+
+			<Button>Select</Button>
+		</li>
+	)
+}
+
+function Button({ children }) {
+	return <button className='button'>{children}</button>
+}
+
+function FormAddFriend() {
+	return (
+		<form className='form-add-friend'>
+			<label>Friend name</label>
+			<input type='text' />
+
+			<label>🌄 image URL</label>
+			<input type='text' />
+
+			<Button>Type something</Button>
+		</form>
+	)
 }
